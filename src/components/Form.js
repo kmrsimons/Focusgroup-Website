048fe39cbded98356dddf70332f4bc1e4b1f0b3f
kmrsimons <106679986+kmrsimons@users.noreply.github.com>
2022-06-15 16:58:07 +0200

    New version

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,45 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+
 export const Form = () => {
-    return (
-        <div>
-            <form>
-                <label for="Firstname">First name:</label><br />
-                <input type="text" id="Firstname" name="Firstname" /><br />
-                <label for="Lastname">Last name:</label><br />
-                <input type="text" id="Lastname" name="Lastname" /><br />
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log('Thanks!')
+    console.log(errors);
+
+    return(
+        <div class="formfield">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div class="buttonfield">
+                    <input type="text" placeholder="First name" {...register("First name", { required: true, maxLength: 80 })} /><br />
+                </div>
                 <br />
-                <input type="radio" id="html" name="fav_language" value="HTML"/>
-                <label for="html">HTML</label><br/>
-                <input type="radio" id="css" name="fav_language" value="CSS"/>
-                <label for="css">CSS</label><br/>
-                <input type="radio" id="javascript" name="fav_language" value="JavaScript"/>
-                <label for="javascript">JavaScript</label><br />
+                <div class="buttonfield">
+                    <input type="text" placeholder="Last name" {...register("Last name", { required: true, maxLength: 100 })} /><br />
+                </div>
                 <br />
-                <input type="submit" value="Submit" />
+                <div class="buttonfield">
+                    <input type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} /><br />
+                </div>
+                <br />
+                <div class="buttonfield-big">
+                    <p>Are you a Developer?</p>
+                    <p>Yes     No</p>
+                    <input {...register("Developer", { required: true })} type="radio" value="Yes" />
+                    <input {...register("Developer", { required: true })} type="radio" value="No" />
+                </div>
+                <br />
+                <div class="buttonfield-big">
+                    <p>What is your Favorite color?</p>
+                        <p>Green Red Blue</p>
+                        <input {...register("Favorite color?")} type="radio" value="Green" class="option" />
+                        <input {...register("Favorite color?")} type="radio" value="Red" class="option" />
+                        <input {...register("Favorite color?")} type="radio" value="Blue" class="option" />
+                </div>
+                <br />
+                <div class="buttonfield">
+                <input type="submit" value="Submit" onSubmit={onSubmit} />
+                </div>
             </form>
         </div>
     )
